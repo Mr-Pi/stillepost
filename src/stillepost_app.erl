f3b2b05e4b417f730ddf66d34ffcfe17ca182b2c
Mr. Pi <github@mr-pi.de>
2013-11-21 02:06:02 +0100

    basic server skeleton, index.html

---
+++ src/stillepost_app.erl
@@ -0,0 +1,16 @@
+-module(stillepost_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    stillepost_sup:start_link().
+
+stop(_State) ->
+    ok.
